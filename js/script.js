bfa483f55989167d7d28919cf76958750c5a21bb
Andrea Esposito <andexpox@gmail.com>
2021-04-28 16:41:14 +0200

    albums

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@
 var app = new Vue ({
     el: '#root',
     data: {
-        
+        musicCd: []
     },
     methods: {
 
     },
     mounted() {
-        
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((response) => {
+                const result = response.data.response;
+                this.musicCd = result;
+            });
     }
 });
